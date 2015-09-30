d57d96fe0fc0e25298506330f35b633914a12168
Tikochato <daor01@gmail.com>
2015-09-30 02:14:42 -0600

    Funcionalidad de busqueda implementada en museos

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,5 @@
 angular.module('MusaGT.controllers', [
-  'MusaGT.services'
-  , 'ionic.contrib.ui.hscrollcards'
+  'MusaGT.services', 'ionic.contrib.ui.hscrollcards'
 ])
 
 .controller('MapaCtrl', function($scope) {
@@ -10,21 +9,24 @@ angular.module('MusaGT.controllers', [
 .controller('MuseosCtrl', function($scope, $state, Museos) {
     $scope.showSearchBar = false;
     var museos = Museos.all();
-    for (i=0; i<museos.length; i++){
+    for (i = 0; i < museos.length; i++) {
       museos[i].tipoMuseo = Museos.getTipo(museos[i].tipoMuseo).tipo;
     }
-    $scope.museos1 = museos.slice(0, (museos.length/2));
-    $scope.museos2 = museos.slice((museos.length/2), (museos.length));
+    $scope.museos1 = museos.slice(0, (museos.length / 2));
+    $scope.museos2 = museos.slice((museos.length / 2), (museos.length));
 
     $scope.$on('$ionicView.enter', function(e) {
       $scope.museos = Museos.all();
+      $scope.searchBar.searchText = "";
     });
 
     /**
       Ir a museo individual
     */
-    $scope.irMuseo = function(idMuseo){
-      $state.go("tab.museos-detail",{museoId:idMuseo});
+    $scope.irMuseo = function(idMuseo) {
+      $state.go("tab.museos-detail", {
+        museoId: idMuseo
+      });
     }
 
     /**
@@ -33,14 +35,16 @@ angular.module('MusaGT.controllers', [
     $scope.searchBar = function() {
       if ($scope.showSearchBar) {
         $scope.showSearchBar = false;
+        $scope.searchBar.searchText = "";
       } else {
         $scope.showSearchBar = true;
       }
     };
+
   }) //MuseosCtrl
 
 .controller('MuseoCtrl', function($scope, $state, $stateParams, $ionicPopover, Museos) {
-  console.log("param: "+$stateParams.museoId);
+    console.log("param: " + $stateParams.museoId);
     $scope.museo = Museos.get($stateParams.museoId);
     $scope.tipoMuseo = Museos.getTipo($scope.museo.tipoMuseo);
     $scope.vm = this;
@@ -57,12 +61,17 @@ angular.module('MusaGT.controllers', [
 .controller('EventosCtrl', function($scope) {
   $scope.showSearchBar = false;
 
+  $scope.$on('$ionicView.enter', function(e) {
+    $scope.searchBar.searchText = "";
+  });
+
   /**
     Metodo que muestra/oculta la barra de búsqueda
   */
   $scope.searchBar = function() {
     if ($scope.showSearchBar) {
       $scope.showSearchBar = false;
+      $scope.searchBar.searchText = "";
     } else {
       $scope.showSearchBar = true;
     }
